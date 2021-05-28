845e90cbf0248c988b72784a20d35ffa4007df84
emrearmagan <emre.armagan@hotmail.de>
2021-05-28 21:21:57 +0200

    init

---
+++ SimpleCharts/SimpleCharts.h
@@ -0,0 +1,18 @@
+//
+//  SimpleCharts.h
+//  SimpleCharts
+//
+//  Created by Emre Armagan on 28.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleCharts.
+FOUNDATION_EXPORT double SimpleChartsVersionNumber;
+
+//! Project version string for SimpleCharts.
+FOUNDATION_EXPORT const unsigned char SimpleChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleCharts/PublicHeader.h>
+
+
---
+++ SimpleChartsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleChartsDemo
+//
+//  Created by Emre Armagan on 28.05.21.
+//
+
+import UIKit
+import SimpleCharts
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
